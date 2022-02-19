2038a3175609a097eb7f4e13357175f029c3f2a7
Illy <user@Users-MacBook-Air.local>
2022-02-19 16:07:32 +0800

    intital commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let singapore = [1.29,103.85]; 
+let map = L.map("main-map").setView(singapore, 13); 
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: "sk.eyJ1IjoiNmlsbHkiLCJhIjoiY2t6dGoxaHJlMGNzZzJvbnduZGdic2lvbyJ9.8s5meHPYaHhf-IOxa03MxA"
+}).addTo(map);
